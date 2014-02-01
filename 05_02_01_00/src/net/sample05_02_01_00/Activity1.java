ea240084bf2741ab1f17a779b6268491b2cdb277
akimaru506 <akatukimaru.rescue@gmail.com>
2014-02-02 03:03:36 +0900

    wa-i

--- 05_02_01_00/src/net/sample05_02_01_00/Activity1.java
+++ 05_02_01_00/src/net/sample05_02_01_00/Activity1.java
@@ -17,17 +17,17 @@ public class Activity1 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
 		textView = (TextView) findViewById(R.id.textView1);
 		button = (Button) findViewById(R.id.button1);
+		button2 = (Button) findViewById(R.id.button2);
 
 		button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				i++;
 				if(i==1){
-					button2 = new Button(Activity1.this);
-				    linearLayout.addView(button2);
-					button2.setText("ーッ!");
+					
+					button2.setWidth(50);
+					button2.setHeight(200);
 					button.setText("タ");
 				 }
 				textView.setText(String.valueOf(i));
@@ -40,7 +40,7 @@ public class Activity1 extends Activity {
 				{
 					textView.setText("北斗百裂拳");
 				}else{
-					textView.setText("北斗"+String.valueOf(i)+"裂拳");
+					textView.setText(String.valueOf(i));
 				}
 			}
 			
